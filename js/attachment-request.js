a489d4827ab439e9df999a9ee686f7949cf68cae
weizhou <weizhou@redhat.com>
2013-05-10 18:46:58 +0800

    Added attachment-request.js.

---
+++ js/attachment-request.js
@@ -0,0 +1,11 @@
+function attachment_post() {
+  //Pseudo ajax post for file uploading
+  $('#add-requirement').iframePostForm({
+    json: true,
+    complete: function(response) {
+      if ( response.rc ) {
+    } else {
+    }
+    }
+  })
+}
