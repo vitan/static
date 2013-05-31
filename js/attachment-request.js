783584b7016e0efdf6833e51c65b250eb413bd8c
weizhou <weizhou@redhat.com>
2013-05-31 13:58:18 +0800

    Added function attachment_init().

--- js/attachment-request.js
+++ js/attachment-request.js
@@ -9,4 +9,26 @@ function attachment_post() {
       }
     }
   })
+};
+
+function attachment_delete(selector) {
+  $.ajax({
+    url: $(selector).attr('href'),
+    type: 'POST',
+    dataType: 'json',
+    success: function (response) {
+      if ( response.rc == 0 ) {
+        alert(response.data.success);
+      } else {
+      }
+    }
+  });
+};
+
+function attachment_init() {
+        attachment_post();
+        $('.attachment-delete').live('click', function() {
+            event.preventDefault();
+            attachment_delete($(this));
+    });
 }
